51910434dd86db8de4cef88375403eac9f4f9249
Hileni89 <138516315+Hileni89@users.noreply.github.com>
2024-05-20 21:14:27 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = math.round(temperature);
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 function searchCity(city) {
-  let apiKey = "f2ad284b00836b5c221ff68a06e0c4fc";
+  let apiKey = "1a747f2d7ac32a100bt13fab8776o6ca";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
