d5b1628784e745be3e58c2605155e553b889a27e
Hileni89 <138516315+Hileni89@users.noreply.github.com>
2024-05-22 10:55:13 +0200

    added the API icon weather to index.html and index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -28,7 +30,11 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `${days} ${hours} :${minutes}`;
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours} :${minutes}`;
 }
 
 function searchCity(city) {
